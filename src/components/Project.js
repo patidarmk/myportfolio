7c00046d35e276d1f23a47088791535d2cb200ce
unknown <patidarmukesh123@gmail.com>
2023-06-16 15:25:15 +0530

    added close button

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,8 @@ import { memo } from "react";
 import harryPotterPreview from "../assets/harry-potter.PNG";
 import spotifyPreview from "../assets/spotify_preview.PNG";
 import ticTactToePreview from "../assets/tic-tac-toe.PNG";
+import curdPreview from "../assets/curd_MERN.PNG";
+
 const Project = memo(() => {
   return (
     <div className="projects-container">
@@ -75,6 +77,8 @@ const Project = memo(() => {
             </div>
           </div>
         </div>
+		
+		
 
         <div className="project">
           <div className="project-details project-details-left">
@@ -140,10 +144,11 @@ const Project = memo(() => {
             <img src={harryPotterPreview} alt="project cover4" />
           </div>
         </div>
-
-        <div className="project">
+		
+		
+		<div className="project">
           <div className="project-img">
-            <img src={ticTactToePreview} alt="project cover 3" />
+            <img src={curdPreview} alt="project-cover" />
           </div>
           <div className="project-details">
             <span className="project-feature-tag">Feature Project</span>
@@ -151,26 +156,93 @@ const Project = memo(() => {
               className="project-title"
               target="_blank"
               rel="noreferrer"
-              href="https://patidarmk-tic-tac-toe.netlify.app/"
+              href="https://patidarmk-spotify.netlify.app/"
             >
-              Tic-Tac-Toe
+              CURD Using MERN
             </a>
             <div className="project-description">
-              Tic-tac-toe is played on a three-by-three grid by two players, who
-              alternately place the marks X and O in one of the nine spaces in
-              the grid.
-            </div>
+					It demonstrates my skills in frontend development with React.js, state management with Redux, server-side development with Node.js and Express.js, and database management with MongoDB.</div>
             <div className="project-skills">
-              <span>HTML</span>
-              <span>CSS</span>
-              <span>Javascript</span>
+              <span>React JS</span>
+              <span>Redux</span>
+			  <span>Node.js</span>
+              <span>Express.js</span>
+			  <span>Express.js</span>
+			  <span>MongoDB</span>
             </div>
             <div className="project-head">
               <div className="icon-container">
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://github.com/patidarmk/tic-tac-toe/tree/master"
+                  href="https://github.com/patidarmk/upforceTech_task_curd"
+                >
+                  <svg
+                    className="icon-secondary"
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 30 30"
+                  >
+                    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                  </svg>
+                </a>
+
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://upforcetech-task.netlify.app/"
+                >
+                  {" "}
+                  <svg
+                    className="icon-secondary"
+                    xmlns="http://www.w3.org/2000/svg"
+                    x="0px"
+                    y="0px"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 30 30"
+                  >
+                    <path d="M 25.980469 2.9902344 A 1.0001 1.0001 0 0 0 25.869141 3 L 20 3 A 1.0001 1.0001 0 1 0 20 5 L 23.585938 5 L 13.292969 15.292969 A 1.0001 1.0001 0 1 0 14.707031 16.707031 L 25 6.4140625 L 25 10 A 1.0001 1.0001 0 1 0 27 10 L 27 4.1269531 A 1.0001 1.0001 0 0 0 25.980469 2.9902344 z M 6 7 C 4.9069372 7 4 7.9069372 4 9 L 4 24 C 4 25.093063 4.9069372 26 6 26 L 21 26 C 22.093063 26 23 25.093063 23 24 L 23 14 L 23 11.421875 L 21 13.421875 L 21 16 L 21 24 L 6 24 L 6 9 L 14 9 L 16 9 L 16.578125 9 L 18.578125 7 L 16 7 L 14 7 L 6 7 z"></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="project">
+          <div className="project-img">
+            <img src={curdPreview} alt="project cover 3" />
+          </div>
+          <div className="project-details">
+             <span className="project-feature-tag">Feature Project</span>
+            <a
+              className="project-title"
+              target="_blank"
+              rel="noreferrer"
+              href="https://patidarmk-spotify.netlify.app/"
+            >
+              CURD Using MERN
+            </a>
+            <div className="project-description">
+					It demonstrates my skills in frontend development with React.js, state management with Redux, server-side development with Node.js and Express.js, and database management with MongoDB.</div>
+            <div className="project-skills">
+              <span>React JS</span>
+              <span>Redux</span>
+			  <span>Node.js</span>
+              <span>Express.js</span>
+			  <span>Express.js</span>
+			  <span>MongoDB</span>
+            </div>
+            <div className="project-head">
+              <div className="icon-container">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/patidarmk/upforceTech_task_curd"
                 >
                   <svg
                     className="icon-secondary"
@@ -187,7 +259,7 @@ const Project = memo(() => {
                 <a
                   target="_blank"
                   rel="noreferrer"
-                  href="https://patidarmk-tic-tac-toe.netlify.app/"
+                 href="https://upforcetech-task.netlify.app/"
                 >
                   <svg
                     className="icon-secondary"
